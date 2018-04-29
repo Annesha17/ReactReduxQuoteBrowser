026c6a7da148287ee512eb57e4b1138ae2c06156
annesha <annesha.k17@gmail.com>
2018-04-29 22:31:25 +0530

    modified fetchQuoteOfTheDay service to shape the response object

--- src/components/services/QuoteService.js
+++ src/components/services/QuoteService.js
@@ -5,6 +5,14 @@ const API_BASE_URL = 'https://favqs.com/api';
 const API_KEY = "d918598bb7bbfcfc8a84bf9e43bd7009";
 //process.env.FAVQS_API_KEY;
 
+const mapQuoteOfTheDay=(quote)=>({
+    quoteOfTheDay:{
+        author:quote.author,
+        content: quote.body,
+        tags: [...quote.tags]
+    }
+})
+
 const requestConfig = {
     headers: {
         Authorization: `Token token="${API_KEY}"`
@@ -17,7 +25,13 @@ export const fetchQuoteOfTheDay = () =>{
     return new Promise((resolve,reject)=>{
         axios.get(url,requestConfig)
         .then((response)=>response.data)
-        
+        .then((quote)=> resolve(mapQuoteOfTheDay(quote)))
+        .catch((error)=>
+        {
+            console.log(error);
+            return reject(error);
+        }
+        )
 
     })
 
